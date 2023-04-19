4b91a7b23fc1b2573cebf04c3701bde34c1f1035
SlavikChyzh <110846906+SlavikChyzh@users.noreply.github.com>
2023-04-19 18:54:55 +0300

    start
    st

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,16 @@
-import { Component } from 'react';
-import ContactForm from './ContactForm/ContactForm';
-import { nanoid } from 'nanoid';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
-import { Conteiner } from './Element/Element.styled';
-
-export default class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-
-  componentDidMount() {
-    if (localStorage.getItem('contacts')) {
-      this.setState({
-        contacts: JSON.parse(localStorage.getItem('contacts')),
-      });
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = cont => {
-    const newContact = {
-      ...cont,
-      id: nanoid(),
-    };
-    this.setState(prevState => {
-      return { contacts: [...prevState.contacts, newContact] };
-    });
-  };
-  deleteContact = id => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(contact => contact.id !== id),
-      };
-    });
-  };
-  onChange = ({ target }) => {
-    this.setState({ filter: target.value });
-  };
-  filterContacts = (arr, filter) => {
-    return arr.filter(ell =>
-      ell.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  };
-
-  render() {
-    return (
-      <Conteiner>
-        <h3>PhoneBook</h3>
-        <ContactForm
-          contacts={this.state.contacts}
-          addContact={this.addContact}
-        ></ContactForm>
-        <Filter onChange={this.onChange} filter={this.state.filter}></Filter>
-        <h3>Contacts</h3>
-        <ContactList
-          contacts={this.filterContacts(this.state.contacts, this.state.filter)}
-          deleteContact={this.deleteContact}
-        ></ContactList>
-      </Conteiner>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
--- src/components/ContactItem/ContactItem.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const ContactItemLi = styled.li`
-  display: flex;
-  list-style: none;
-  flex-direction: column;
-`;
-
-export { ContactItemLi };
